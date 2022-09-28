c771477c53c05276da8084231ddab8780b368d22
Loïc Chagnoleau <lchagnoleau@pm.me>
2022-09-28 10:05:31 +0200

    ask a guessing number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please enter an number between 1 and 100:");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("You entered: {guess}");
 }
