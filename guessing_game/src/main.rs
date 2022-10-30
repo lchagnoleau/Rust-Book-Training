6fe1aae6758553408b0a72355cdc6cbc38a6a29c
Loïc Chagnoleau <lchagnoleau@pm.me>
2022-10-30 17:53:23 +0100

    9.3 improve guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,10 +2,28 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must between 1 and 100. Got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = Guess::new(rand::thread_rng().gen_range(1..=100)).value();
 
     loop {
         println!("Please enter an number between 1 and 100:");
@@ -15,10 +33,10 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read line");
 
-        let guess: u8 = match guess
+        let guess: i32 = match guess
             .trim() // Delete all whitespace, newline etc...
             .parse() { // Convert String to the type of u8
-                Ok(num) => num,
+                Ok(num) => Guess::new(num).value(),
                 Err(_) => continue,
             };
 
